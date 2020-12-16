35824cafb7448aa8fe868e6bb4f940de7be412d1
Wan-Teh Chang <wtc@google.com>
2020-12-15 18:46:50 -0800

    Make tests_utils_test depend on absl::time
    ${libgav1_root}/tests/utils.cc uses absl::time, so tests_utils_test needs to
    depend on absl::time. tests_utils_test does not seem to need absl::base.
    
    This linker error can be reproduced by configuring cmake with
    -DLIBGAV1_THREADPOOL_USE_STD_MUTEX=1.
    
    TEST=cmake .. -DLIBGAV1_THREADPOOL_USE_STD_MUTEX=1
    PiperOrigin-RevId: 347735486
    Change-Id: I3c35975eadecb474b78f4e0177f444d649f74098

--- tests/libgav1_tests.cmake
+++ tests/libgav1_tests.cmake
@@ -125,7 +125,7 @@ macro(libgav1_add_tests_targets)
                          libgav1_tests_utils
                          libgav1_utils
                          LIB_DEPS
-                         absl::base
+                         absl::time
                          ${libgav1_common_test_absl_deps}
                          libgav1_gtest
                          libgav1_gtest_main)
