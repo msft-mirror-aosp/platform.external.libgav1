37d638bf274a36962e2ea018a37bae5c030cc01b
James Zern <jzern@google.com>
2020-12-21 11:35:39 -0800

    distance_weighted_blend_test,cosmetics: 10bppp -> 10bpp
    PiperOrigin-RevId: 348500233
    Change-Id: Ia8cd99b62dd0aa1b32f93fd720b14d104e5e68e2

--- src/dsp/distance_weighted_blend_test.cc
+++ src/dsp/distance_weighted_blend_test.cc
@@ -273,17 +273,17 @@ const char* GetDistanceWeightedBlendDigest10bpp(const TestParam block_size) {
   }
 }
 
-using DistanceWeightedBlendTest10bppp = DistanceWeightedBlendTest<10, uint16_t>;
+using DistanceWeightedBlendTest10bpp = DistanceWeightedBlendTest<10, uint16_t>;
 
-TEST_P(DistanceWeightedBlendTest10bppp, Blending) {
+TEST_P(DistanceWeightedBlendTest10bpp, Blending) {
   Test(GetDistanceWeightedBlendDigest10bpp(GetParam()), 1);
 }
 
-TEST_P(DistanceWeightedBlendTest10bppp, DISABLED_Speed) {
+TEST_P(DistanceWeightedBlendTest10bpp, DISABLED_Speed) {
   Test(GetDistanceWeightedBlendDigest10bpp(GetParam()), kNumSpeedTests);
 }
 
-INSTANTIATE_TEST_SUITE_P(C, DistanceWeightedBlendTest10bppp,
+INSTANTIATE_TEST_SUITE_P(C, DistanceWeightedBlendTest10bpp,
                          ::testing::ValuesIn(kTestParam));
 #endif  // LIBGAV1_MAX_BITDEPTH >= 10
 
