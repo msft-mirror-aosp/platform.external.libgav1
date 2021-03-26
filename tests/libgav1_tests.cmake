79cfc3ddaeaa9638dd06f344e4f8a0964a4b023c
Alex Peterson <petersonab@google.com>
2021-03-25 20:02:52 -0700

    Add intrapred_filter_test to cmake executables
    PiperOrigin-RevId: 365176304
    Change-Id: I0d16062f922c93f14e781857dad4d4cde752412d

--- tests/libgav1_tests.cmake
+++ tests/libgav1_tests.cmake
@@ -312,6 +312,27 @@ macro(libgav1_add_tests_targets)
 
   libgav1_add_executable(TEST
                          NAME
+                         intrapred_filter_test
+                         SOURCES
+                         ${libgav1_intrapred_filter_test_sources}
+                         DEFINES
+                         ${libgav1_defines}
+                         INCLUDES
+                         ${libgav1_test_include_paths}
+                         OBJLIB_DEPS
+                         libgav1_decoder
+                         libgav1_dsp
+                         libgav1_tests_block_utils
+                         libgav1_tests_utils
+                         libgav1_utils
+                         LIB_DEPS
+                         absl::time
+                         ${libgav1_common_test_absl_deps}
+                         libgav1_gtest
+                         libgav1_gtest_main)
+
+  libgav1_add_executable(TEST
+                         NAME
                          intrapred_test
                          SOURCES
                          ${libgav1_intrapred_test_sources}
