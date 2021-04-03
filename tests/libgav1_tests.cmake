75c02908af6181875b1ff4fe85017b6868560101
James Zern <jzern@google.com>
2021-04-02 22:20:24 -0700

    add motion_field_projection_test.cc
    PiperOrigin-RevId: 366565809
    Change-Id: Iab2d19725a733f77d18eb22406914404ce63b059

--- tests/libgav1_tests.cmake
+++ tests/libgav1_tests.cmake
@@ -95,6 +95,8 @@ list(APPEND libgav1_loop_restoration_test_sources
             "${libgav1_source}/dsp/loop_restoration_test.cc")
 list(APPEND libgav1_mask_blend_test_sources
             "${libgav1_source}/dsp/mask_blend_test.cc")
+list(APPEND libgav1_motion_field_projection_test_sources
+            "${libgav1_source}/dsp/motion_field_projection_test.cc")
 list(APPEND libgav1_weight_mask_test_sources
             "${libgav1_source}/dsp/weight_mask_test.cc")
 list(APPEND libgav1_obmc_test_sources "${libgav1_source}/dsp/obmc_test.cc")
@@ -473,6 +475,27 @@ macro(libgav1_add_tests_targets)
                          mask_blend_test
                          SOURCES
                          ${libgav1_mask_blend_test_sources}
+                         DEFINES
+                         ${libgav1_defines}
+                         INCLUDES
+                         ${libgav1_test_include_paths}
+                         OBJLIB_DEPS
+                         libgav1_decoder
+                         libgav1_dsp
+                         libgav1_tests_utils
+                         libgav1_utils
+                         LIB_DEPS
+                         absl::str_format_internal
+                         absl::time
+                         ${libgav1_common_test_absl_deps}
+                         libgav1_gtest
+                         libgav1_gtest_main)
+
+  libgav1_add_executable(TEST
+                         NAME
+                         motion_field_projection_test
+                         SOURCES
+                         ${libgav1_motion_field_projection_test_sources}
                          DEFINES
                          ${libgav1_defines}
                          INCLUDES
