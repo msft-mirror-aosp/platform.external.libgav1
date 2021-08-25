4226732b536df7a029da7591df278b3658c02138
James Zern <jzern@google.com>
2021-08-25 14:23:52 -0700

    {*_scratch_buffer,dynamic_buffer,obu_parser}.h: apply iwyu
    PiperOrigin-RevId: 392979734
    Change-Id: I759a378d730711eb4b8f253d40f3d24a126708cd

--- src/frame_scratch_buffer.h
+++ src/frame_scratch_buffer.h
@@ -17,10 +17,13 @@
 #ifndef LIBGAV1_SRC_FRAME_SCRATCH_BUFFER_H_
 #define LIBGAV1_SRC_FRAME_SCRATCH_BUFFER_H_
 
+#include <array>
 #include <condition_variable>  // NOLINT (unapproved c++11 header)
 #include <cstdint>
 #include <memory>
 #include <mutex>  // NOLINT (unapproved c++11 header)
+#include <new>
+#include <utility>
 
 #include "src/loop_restoration_info.h"
 #include "src/residual_buffer_pool.h"
