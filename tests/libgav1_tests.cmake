ecf3ab3df1581bb84552aa670f0e973955f8f1cc
James Zern <jzern@google.com>
2021-08-25 11:32:59 -0700

    add film_grain_test.cc
    PiperOrigin-RevId: 392942116
    Change-Id: I6d6b53d61e0cea9034f6d568e17c4def3b5af9db

--- tests/libgav1_tests.cmake
+++ tests/libgav1_tests.cmake
@@ -82,6 +82,8 @@ list(APPEND libgav1_decoder_buffer_test_sources
 list(APPEND libgav1_distance_weighted_blend_test_sources
             "${libgav1_source}/dsp/distance_weighted_blend_test.cc")
 list(APPEND libgav1_dsp_test_sources "${libgav1_source}/dsp/dsp_test.cc")
+list(APPEND libgav1_film_grain_test_sources
+            "${libgav1_source}/film_grain_test.cc")
 list(APPEND libgav1_intra_edge_test_sources
             "${libgav1_source}/dsp/intra_edge_test.cc")
 list(APPEND libgav1_intrapred_cfl_test_sources
@@ -161,6 +163,28 @@ macro(libgav1_add_tests_targets)
   if(use_absl_threading)
     list(APPEND libgav1_common_test_absl_deps absl::synchronization)
   endif()
+
+  libgav1_add_executable(TEST
+                         NAME
+                         film_grain_test
+                         SOURCES
+                         ${libgav1_film_grain_test_sources}
+                         DEFINES
+                         ${libgav1_defines}
+                         INCLUDES
+                         ${libgav1_test_include_paths}
+                         OBJLIB_DEPS
+                         libgav1_decoder
+                         libgav1_dsp
+                         libgav1_tests_block_utils
+                         libgav1_tests_utils
+                         libgav1_utils
+                         LIB_DEPS
+                         absl::str_format_internal
+                         absl::time
+                         ${libgav1_common_test_absl_deps}
+                         libgav1_gtest
+                         libgav1_gtest_main)
 
   libgav1_add_executable(TEST
                          NAME
