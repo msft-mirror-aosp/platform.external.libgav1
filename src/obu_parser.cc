1b8a9103276d5d5bd0d19e735e4e5f20aea2556e
Chunbo Hua <chunbo.hua@intel.com>
2024-03-13 23:22:03 +0800

    obu_parser: Frame header check for new sequence header OBU
    According to AV1 spec Section 7.5. Ordering of OBUs, a new coded
    video sequence is defined to start at each temporal unit which
    satisfies both of the following conditions:
      * A sequence header OBU appears before the first frame header.
      * The first frame header has frame_type equal to KEY_FRAME,
        show_frame equal to 1, show_existing_frame equal to 0, and
        temporal_id equal to 0.
    
    For the consideration of decoding compatibility as libaom does, it
    is defined as warning in order to make the decoding job be continued.
    
    Change-Id: Icbc18d6c69c3b9da9ee9ab4ae86e41f60f524055
    Reviewed-on: https://chromium-review.googlesource.com/c/codecs/libgav1/+/5369036
    Reviewed-by: James Zern <jzern@google.com>
    Tested-by: WebM Builds <builds@webmproject.org>
    Reviewed-by: Vignesh Venkat <vigneshv@google.com>

--- src/obu_parser.cc
+++ src/obu_parser.cc
@@ -2191,6 +2191,24 @@ bool ObuParser::ParseFrameHeader() {
   if (sequence_header_.film_grain_params_present) {
     current_frame_->set_film_grain_params(frame_header_.film_grain_params);
   }
+  if (sequence_header_changed_ &&
+      (frame_header_.frame_type != kFrameKey ||
+       !frame_header_.show_frame ||
+       frame_header_.show_existing_frame ||
+       current_frame_->temporal_id() != 0)) {
+    // Section 7.5. Ordering of OBUs: A new coded video sequence is defined to
+    // start at each temporal unit which satisfies both of the following
+    // conditions:
+    //   * A sequence header OBU appears before the first frame header.
+    //   * The first frame header has frame_type equal to KEY_FRAME, show_frame
+    //     equal to 1, show_existing_frame equal to 0, and temporal_id equal to
+    //     0.
+    LIBGAV1_DLOG(WARNING,
+        "The first frame successive to sequence header OBU should be a "
+        "keyframe with show_frame=1, show_existing_frame=0 and "
+        "temporal_id=0");
+  }
+
   return true;
 }
 
